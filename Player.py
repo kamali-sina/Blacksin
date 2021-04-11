d0a84f48b8c4f1548d4dd1e699fcc1f5837e18e7
sinku <sininoir@gmail.com>
2021-04-11 13:45:23 +0430

    fixed bug in new cpu ai

--- Player.py
+++ Player.py
@@ -27,15 +27,25 @@ class Player:
         return mean_of_remaining_cards
     
     def play(self, seen_cards, expert_mode, deck, enemies_cards):
+        if (len(deck) > 0):
+            next_card_in_deck = deck[0]
+        else:
+            next_card_in_deck = 0
+        if (len(deck) > 1):
+            next_enemy_card_in_deck = deck[1]
+        else:
+            next_enemy_card_in_deck = 0
         amount_to_target = self.target - sum(self.cards)
-        amount_with_next_card = self.target - (sum(self.cards) + deck[0])
+        amount_with_next_card = self.target - (sum(self.cards) + next_card_in_deck)
         enemies_amount_to_target = self.target - sum(enemies_cards)
-        enemies_amount_with_next_card = self.target - (sum(enemies_cards) + deck[1])
-        _stop_condition = amount_to_target < deck[0] and self.erases_remaining <= 0
-        _draw_condition_1 = amount_with_next_card <= enemies_amount_with_next_card
+        enemies_amount_with_next_card = self.target - (sum(enemies_cards) + next_enemy_card_in_deck)
+        print(next_card_in_deck)
+        _stop_condition = amount_to_target < next_card_in_deck and self.erases_remaining <= 0
+        _draw_condition_1 = next_card_in_deck != 0
         _draw_condition_2 = amount_with_next_card >= 0
+        print(amount_with_next_card)
         _erase_condition = self.erases_remaining > 0 and expert_mode
-        _erase_self_condition = amount_with_next_card < 0
+        _erase_self_condition = amount_to_target < 0
         _erase_opponent_condition_or = enemies_amount_to_target < (self.target //7)
         _erase_opponent_condition_or_2 = enemies_amount_with_next_card < (self.target // 7) 
         _erase_opponent_condition_or_3 = enemies_amount_with_next_card <= amount_with_next_card
@@ -43,6 +53,7 @@ class Player:
         _erase_opponent_condition = _erase_opponent_condition_or or _erase_opponent_condition_or_2 or _erase_opponent_condition_or_3
         _erase_opponent_condition = _erase_opponent_condition or _erase_opponent_condition_or_4 
         if (_stop_condition): 
+            print('stop 1')
             return 'stop'
         elif (_draw_condition_1 and _draw_condition_2):
             return 'draw'
@@ -51,6 +62,7 @@ class Player:
         elif(_erase_opponent_condition and _erase_condition):
             return 'erase_opponent'
         else:
+            print('stop 2')
             return 'stop'
     
     def erase(self, target):
